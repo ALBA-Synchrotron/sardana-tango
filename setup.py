091d95a759220982f6a61c473ab7f1966847a2fc
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-03-15 19:04:22 +0100

    Use setuptools find_packages() helper

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from datetime import datetime
 
 release = datetime.today().strftime('%Y.%m.%d.%H.%M').replace(".0", ".")
-
-package_dir = {"sardana.PoolController.sardana_tango": "sardana_tango/ctrl"}
-
-packages = ["sardana.PoolController.sardana_tango"]
-
-provides = ['sardana_tango']
-
 
 setup(name='sardana_tango',
       version=release,
@@ -19,8 +12,5 @@ setup(name='sardana_tango',
       maintainer="ALBA",
       maintainer_email="sardana-devel@lists.sourceforge.net",
       url="https://github.com/ALBA-Synchrotron/sardana-tango",
-      packages=packages,
-      package_dir=package_dir,
-      include_package_data=True,
-      provides=provides,
-      )
+      packages=find_packages()
+)

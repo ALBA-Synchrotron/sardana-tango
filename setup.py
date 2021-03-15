8964e7ab021dbfdc41c53a027936d19ef3fca2fa
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-03-15 19:11:16 +0100

    Add description, long description, license, etc

--- setup.py
+++ setup.py
@@ -3,14 +3,25 @@
 from setuptools import setup, find_packages
 from datetime import datetime
 
+with open("README.md") as f:
+    readme = f.read()
+
 release = datetime.today().strftime('%Y.%m.%d.%H.%M').replace(".0", ".")
 
-setup(name='sardana_tango',
-      version=release,
-      author="Sardana Controller Developers",
-      author_email="sardana-devel@lists.sourceforge.net",
-      maintainer="ALBA",
-      maintainer_email="sardana-devel@lists.sourceforge.net",
-      url="https://github.com/ALBA-Synchrotron/sardana-tango",
-      packages=find_packages()
+setup(
+    name='sardana_tango',
+    version=release,
+    author="Sardana Controller Developers",
+    author_email="sardana-devel@lists.sourceforge.net",
+    maintainer="ALBA",
+    maintainer_email="sardana-devel@lists.sourceforge.net",
+    url="https://github.com/ALBA-Synchrotron/sardana-tango",
+    packages=find_packages(),
+    description="sardana tango controllers & macros",
+    license="GPLv3",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="tango,sardana",
+    python_requires=">=3.5"
 )

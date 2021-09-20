ALBA-Synchrotron/sardana-tango

commit 1e6515b1acc7cdb29f392184e69f230f25cc1fbe

Fix TangoAttrCTCtrl to read all attributes on all devices

sardana_tango/ctrl/TangoAttrCTCtrl.py
@@ -87,28 +87,30 @@ class ReadTangoAttributes:
                     self._log.error(
                         'Exception reading attributes:%s.%s' % (
                             dev, str(attributes)))
-            for attr in attributes:
-                axies = []
-                for axis, dic in self.devsExtraAttributes.items():
-                    if dic[TANGO_ATTR] == dev + '/' + attr:
-                        axies.append(axis)
-                for axis in axies:
-                    if len(values) > 0:
-                        dev_attr_value = values[attr]
-                        if dev_attr_value.has_failed:
-                            # In case of Attribute error
-                            VALUE = tango.DevFailed(
-                                *dev_attr_value.get_err_stack())
-                            self.devsExtraAttributes[axis][EVALUATED_VALUE] = \
-                                VALUE
-                        else:
-                            formula = self.devsExtraAttributes[axis][FORMULA]
-                            VALUE = float(dev_attr_value.value)
-                            # just in case 'VALUE' has been written
-                            # in lowercase...
-                            value = VALUE  # noqa: F841
-                            v = eval(formula)
-                            self.devsExtraAttributes[axis][EVALUATED_VALUE] = v
+                for attr in attributes:
+                    axies = []
+                    for axis, dic in self.devsExtraAttributes.items():
+                        if dic[TANGO_ATTR] == dev + '/' + attr:
+                            axies.append(axis)
+                    for axis in axies:
+                        if len(values) > 0:
+                            dev_attr_value = values[attr]
+                            self._log.debug("For attribute %s axis %d [%s]"
+                                            % (str(attr), axis, str(dev_attr_value)))
+                            if dev_attr_value.has_failed:
+                                # In case of Attribute error
+                                VALUE = tango.DevFailed(
+                                    *dev_attr_value.get_err_stack())
+                                self.devsExtraAttributes[axis][EVALUATED_VALUE] = \
+                                    VALUE
+                            else:
+                                formula = self.devsExtraAttributes[axis][FORMULA]
+                                VALUE = float(dev_attr_value.value)
+                                # just in case 'VALUE' has been written
+                                # in lowercase...
+                                value = VALUE  # noqa: F841
+                                v = eval(formula)
+                                self.devsExtraAttributes[axis][EVALUATED_VALUE] = v
         except Exception as e:
             self._log.error('Exception on read_all: %r' % e)
 

commit f411725af283473cceca7299590e8d3a17695bf4

Remove too talkative log from TangoAttrCTCtrl
@jairomoldes discovered that TangoAttrCTCtrl is too talkative.
1e6515b1acc7c accidentally left a log message which is not necessary - remove it.

sardana_tango/ctrl/TangoAttrCTCtrl.py
@@ -96,9 +96,6 @@ class ReadTangoAttributes:
                         extra_attribute_axis = self.devsExtraAttributes[axis]
                         if len(values) > 0:
                             dev_attr_value = values[attr]
-                            self._log.debug("For attribute %s axis %d [%s]"
-                                            % (str(attr), axis,
-                                               str(dev_attr_value)))
                             if dev_attr_value.has_failed:
                                 # In case of Attribute error
                                 VALUE = tango.DevFailed(